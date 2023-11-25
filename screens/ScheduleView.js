45444daf3f08e86422343fbda7cbf109d389fafe
strongcookdas <goodjav141592@gmail.com>
2023-11-26 01:44:07 +0900

    style : 일정 뷰 스타일 수정

--- screens/ScheduleView.js
+++ screens/ScheduleView.js
@@ -1,7 +1,8 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { Calendar } from "react-native-big-calendar";
 import { useScheduleContext } from "../hooks/ScheduleProvider";
 import { useEffect, useState } from "react";
+import { Colors } from "../constant/colors";
 
 function ScheduleView() {
   const { scheduleData } = useScheduleContext();
@@ -12,9 +13,26 @@ function ScheduleView() {
   }, [scheduleData]);
   return (
     <View>
-      <Calendar events={events} height={600} />
+      <Calendar
+        events={events}
+        height={600}
+        eventCellStyle={styles.eventCellStyle}
+        scrollViewStyle={styles.scrollViewStyle}
+      />
     </View>
   );
 }
 
 export default ScheduleView;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  eventCellStyle: {
+    backgroundColor: Colors.color50,
+    borderRadius: 5,
+  },
+});
